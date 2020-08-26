459dc113042ddb12868fe42171482e96fde8b985
code-lad <codelad1@gmial.com>
2020-08-26 11:34:46 +0200

    vue skills app dynamic content

--- src/views/AddSkills.vue
+++ src/views/AddSkills.vue
@@ -2,37 +2,47 @@
   <div id="add-skills">
     <div class="container">
       <form @submit.prevent="AddSkills">
-        <div class="field title">
-          <label for="title">Skill Title:</label>
-          <input type="text" autocomplete="off" name="title" v-model="title" />
-        </div>
-        <div v-for="(skill, index) in skills" :key="index">
-          <label for="skill">Add Skills</label>
-          <input type="text" name="skill" v-model="skills[index]" />
-          <span class="chip">
-            {{skills[index]}}
-            <i class="material-icons">clear</i>
-          </span>
-        </div>
-        <div class="field add-skills">
-          <label for="add-skills">Add Skills:</label>
-          <input
-            type="text"
-            autocomplete="off"
-            name="add-skills"
-            @keydown.tab.prevent="addIn"
-            v-model="another"
-          />
-        </div>
-        <div class="field center">
-          <p v-if="feedback" class="red-text">{{feedback}}</p>
-          <button class="btn btn-primary">Add Skills</button>
-        </div>
+        <ul class="list-skills">
+          <li>
+            <div class="field title">
+              <label for="title">Skill Title:</label>
+              <input type="text" autocomplete="off" name="title" v-model="title" />
+              <p v-if="feedback" class="red-text">{{feedback}}</p>
+            </div>
+          </li>
+          <li class="list-skills-list" v-for="(skill, index) in skills" :key="index">
+            <!-- <label for="skill">Add Skills</label>
+            <input type="text" name="skill" v-model="skills[index]" />-->
+            <span class="chip">
+              <span class="skill-span">{{skills[index]}}</span>
+              <i class="material-icons remove" @click="deleteSkillsList(skills.id)">clear</i>
+            </span>
+          </li>
+          <li>
+            <div class="field add-skills">
+              <label for="add-skills">Add Skills:</label>
+              <input
+                type="text"
+                autocomplete="off"
+                name="add-skills"
+                @keydown.tab.prevent="addIn"
+                v-model="another"
+              />
+            </div>
+            <p v-if="feedback" class="red-text">{{feedback}}</p>
+          </li>
+          <li>
+            <div class="field center">
+              <button class="btn btn-primary">Add Skills</button>
+            </div>
+          </li>
+        </ul>
       </form>
     </div>
   </div>
 </template>
 <script>
+import db from "@/firebase/init";
 export default {
   name: "AddSkills",
   data() {
@@ -41,11 +51,22 @@ export default {
       another: null,
       skills: [],
       feedback: "",
+      slug: null,
     };
   },
   methods: {
     AddSkills() {
       console.log(this.title, this.skills);
+      if (this.title) {
+        this.feedback = null;
+        db.collection("knacks").add({
+          title: "",
+          skills: "",
+          slug: "",
+        });
+      } else {
+        this.feedback = "Enter valid skills title";
+      }
     },
     addIn() {
       if (this.another) {
@@ -57,6 +78,11 @@ export default {
         this.feedback = "Please enter valid value";
       }
     },
+    deleteSkillsList(id) {
+      this.skills = this.skills.filter((skills) => {
+        return skills.id != id;
+      });
+    },
   },
 };
 </script>
@@ -64,5 +90,25 @@ export default {
 #add-skills {
   max-width: 500px;
   margin: 40px auto;
+}
+#add-skills .chip {
+  position: relative;
+  margin-top: 10px;
+}
+#add-skills .skill-span {
+  font-size: medium;
+}
+#add-skills .remove {
+  font-size: 20px;
+  position: absolute;
+  top: -10px;
+  background-color: brown;
+  border-radius: 10px;
+  color: white;
+  cursor: pointer;
+}
+#add-skills .list-skills .list-skills-list {
+  display: inline-block;
+  margin-right: 10px;
 }
 </style>
